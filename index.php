b83a34c837b7bc019c532dfd5a350f914d9cc238
sabrina <sabrina.pomajulca@tecsup.edu.pe>
2021-02-26 15:38:13 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php include('header.php') ?>
+<div class="container mb-4">
+  <br />
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-5">
+              <img src="img/enchufe.png" alt="..." class="img-fluid" />
+            </div>
+            <div class="col-md-6">
+              <h5 class="card-title">Sensor Eléctrico</h5>
+              <p class="card-text">
+                <i class="fas fa-battery-three-quarters"></i> Normal
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted text-center">
+          <a href="module_status.php">
+            <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+              <i class="fas fa-battery-half"></i>
+            </button>
+          </a>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-quarter"></i>
+          </button>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-three-quarters"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-5">
+              <img src="img/upc.png" alt="..." class="img-fluid" />
+            </div>
+            <div class="col-md-6">
+              <h5 class="card-title">Sensor CPU</h5>
+              <p class="card-text">
+                <i class="fas fa-battery-three-quarters"></i> Normal
+              </p>
+
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted">
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-half"></i>
+          </button>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-quarter"></i>
+          </button>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-three-quarters"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-5">
+              <img src="img/configuraciones.png" alt="..." class="img-fluid" />
+            </div>
+            <div class="col-md-6">
+              <h5 class="card-title">Sensor Eléctrico</h5>
+              <p class="card-text">
+                <i class="fas fa-battery-three-quarters"></i> Normal
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted">
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-half"></i>
+          </button>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-quarter"></i>
+          </button>
+          <button type="button" class="btn btn-outline-secundary btn-floating" data-mdb-ripple-color="dark">
+            <i class="fas fa-battery-three-quarters"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('footer.php') ?>
